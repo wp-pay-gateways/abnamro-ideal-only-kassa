7e183e129d0290ef7d3a187698dae0016ed03d2e
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 08:31:42 +0100

    Extend from abstract Ogone class.

--- src/Integration.php
+++ src/Integration.php
@@ -1,22 +1,10 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_AbnAmro_IDealOnlyKassa_Integration {
+class Pronamic_WP_Pay_Gateways_AbnAmro_IDealOnlyKassa_Integration extends Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_Integration {
 	public function __construct() {
 		$this->id       = 'abnamro-ideal-only-kassa';
 		$this->name     = 'ABN AMRO - iDEAL Only Kassa';
 		$this->url      = 'https://internetkassa.abnamro.nl/';
 		$this->provider = 'abnamro';
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_Config';
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_Gateway';
 	}
 }
