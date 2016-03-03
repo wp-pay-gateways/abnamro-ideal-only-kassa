bd36af866cbcf7fcae07da338ffb469f0dfc40e7
Reüel van der Steege <reuel@pronamic.nl>
2016-03-03 16:53:48 +0100

    Add product and dashboard URLs

--- src/Integration.php
+++ src/Integration.php
@@ -11,9 +11,11 @@
  */
 class Pronamic_WP_Pay_Gateways_AbnAmro_IDealOnlyKassa_Integration extends Pronamic_WP_Pay_Gateways_AbnAmro_Internetkassa_Integration {
 	public function __construct() {
-		$this->id       = 'abnamro-ideal-only-kassa';
-		$this->name     = 'ABN AMRO - iDEAL Only Kassa';
-		$this->url      = 'https://internetkassa.abnamro.nl/';
-		$this->provider = 'abnamro';
+		$this->id            = 'abnamro-ideal-only-kassa';
+		$this->name          = 'ABN AMRO - iDEAL Only Kassa';
+		$this->url           = 'https://internetkassa.abnamro.nl/';
+		$this->product_url   = 'https://www.abnamro.nl/nl/zakelijk/betalen/online-betalen/betaaloplossing/';
+		$this->dashboard_url = 'https://internetkassa.abnamro.nl/';
+		$this->provider      = 'abnamro';
 	}
 }
